3f4af862113d2f07b9706a8c5ce71cbe1c4c752e
Mo'men Soliman <moumensoliman@gmail.com>
2019-02-05 21:20:27 +0200

    Display Item cards

--- frontend/components/item.jsx
+++ frontend/components/item.jsx
@@ -1,12 +1,10 @@
-import React, {
-  Component
-} from "react";
-import PropTypes from "prop-types";
-import Link from "next/link";
-import Title from "./styles/Title";
-import ItemStyles from "./styles/ItemStyles";
-import PriceTag from "./styles/PriceTag";
-import formatMoney from "../lib/formatMoney";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import Title from './styles/Title';
+import ItemStyles from './styles/ItemStyles';
+import PriceTag from './styles/PriceTag';
+import formatMoney from '../lib/formatMoney';
 
 export default class Item extends Component {
   static propTypes = {
@@ -14,44 +12,37 @@ export default class Item extends Component {
   };
 
   render() {
-    const {
-      item
-    } = this.props;
-    return ( <
-      ItemStyles >
-      <
-      Title >
-      <
-      Link href = {
-        {
-          pathname: "/item",
-          query: {
-            id: item.id
-          }
-        }
-      } >
-      <
-      a > {
-        item.title
-      } < /a> <
-      /Link> <
-      /Title> <
-      PriceTag > {
-        formatMoney(item.price)
-      } < /PriceTag> <
-      p > {
-        item.description
-      } < /p>
-
-      <
-      dv className = "buttonList" >
-      <
-      Link >
-      <
-      a > Edit < /a> <
-      /Link> <
-      /dv> <
-      /ItemStyles>
+    const { item } = this.props;
+    return (
+      <ItemStyles>
+        {item.image && <img src={item.image} alt={item.title} />}
+        <Title>
+          <Link
+            href={{
+              pathname: '/item',
+              query: {
+                id: item.id
+              }
+            }}
+          >
+            <a> {item.title} </a>
+          </Link>
+        </Title>
+        <PriceTag> {formatMoney(item.price)} </PriceTag>
+        <p> {item.description} </p>
+        <dv className="buttonList">
+          <Link
+            href={{
+              pathname: 'update',
+              query: { id: item.id }
+            }}
+          >
+            <a> Edit </a>
+          </Link>
+          <button>Add to Cart</button>
+          <button>Delete</button>
+        </dv>
+      </ItemStyles>
     );
   }
 }
