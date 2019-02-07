e9490086683970316fbb34b11ad285d0fe675ed2
Mo'men Soliman <moumensoliman@gmail.com>
2019-02-07 14:34:10 +0200

    Add JWT (JavaScript Web Token)

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,33 @@
-require('dotenv').config({ path: 'variables.env' });
+const cookieParser = require('cookie-parser');
+require('dotenv').config({
+  path: 'variables.env'
+});
+const jwt = require('jsonwebtoken');
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-// TODO Use express middlware to handle cookies (JWT)
-// TODO Use express middlware to populate current user
+// Use express middlware to handle cookies (JWT)
+server.express.use(cookieParser());
+
+// decode the JWT so we can get t he user Id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    //put the userId on to rhe request for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
     cors: {
       credentials: true,
-      origin: process.env.FRONTEND_URL,
-    },
+      origin: process.env.FRONTEND_URL
+    }
   },
   deets => {
     console.log(`Server is now running on port http://localhost:${deets.port}`);
