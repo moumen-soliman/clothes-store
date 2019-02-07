df0aae797db02ac68d951b1c298b431f900aac9d
Mo'men Soliman <moumensoliman@gmail.com>
2019-02-07 14:40:06 +0200

    Create Signin React Component

---
+++ frontend/components/Signin.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Form from './styles/Form';
+import Error from './ErrorMessage';
+import { CURRENT_USER_QUERY } from './User';
+
+const SIGNIN_MUTATION = gql`
+  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
+    signin(email: $email, password: $password) {
+      id
+      email
+      name
+    }
+  }
+`;
+
+export default class Signin extends Component {
+  state = {
+    name: '',
+    password: '',
+    email: ''
+  };
+  saveToState = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  render() {
+    return (
+      <Mutation
+        mutation={SIGNIN_MUTATION}
+        variables={this.state}
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+      >
+        {(signin, { error, loading }) => (
+          <Form
+            method="post"
+            onSubmit={e => {
+              e.preventDefault();
+              this.setState({ email: '', password: '', name: '' });
+            }}
+          >
+            <fieldset>
+              <h2>Sign in into your account</h2>
+              <Error error={error} />
+              <label htmlFor="email">
+                email
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  value={this.state.email}
+                  onChange={this.saveToState}
+                />
+              </label>
+              <label htmlFor="password">
+                password
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  value={this.state.password}
+                  onChange={this.saveToState}
+                />
+              </label>
+
+              <button type="submit">Sign In!</button>
+            </fieldset>
+          </Form>
+        )}
+      </Mutation>
+    );
+  }
+}
