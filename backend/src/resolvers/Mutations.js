68b2a64cb7726833db14d8786c05b8ef8f5415a8
Mo'men Soliman <moumensoliman@gmail.com>
2019-02-07 17:15:33 +0200

    Moving from Mutation to Mutations

--- backend/src/resolvers/Mutations.js
+++ backend/src/resolvers/Mutations.js
@@ -4,12 +4,22 @@ const { randomBytes } = require('crypto');
 const { promisify } = require('util');
 const { transport, makeANiceEmail } = require('../mail');
 
-const Mutation = {
+const Mutations = {
   async createItem(parent, args, ctx, info) {
-    // CCheck if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in to do that!');
+    }
+
+    // Check if they are logged in
     const item = await ctx.db.mutation.createItem(
       {
         data: {
+          // This is how to create a relationship between the Item and the User
+          user: {
+            connect: {
+              id: ctx.request.userId
+            }
+          },
           ...args
         }
       },
@@ -186,4 +196,4 @@ const Mutation = {
   }
 };
 
-module.exports = Mutation;
+module.exports = Mutations;
