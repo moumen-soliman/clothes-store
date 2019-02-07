45de153becf37dabe5b6e2f88135dd34078b8220
Mo'men Soliman <moumensoliman@gmail.com>
2019-02-07 20:43:52 +0200

    Add s to Mutations

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -1,5 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga');
-const Mutations = require('./resolvers/Mutations');
+const Mutations = require('./resolvers/Mutation');
 const Query = require('./resolvers/Query');
 const db = require('./db');
 
