36d4256b1b1c49d521537f4a2f33e21b26b93b44
Mo'men Soliman <moumensoliman@gmail.com>
2019-02-07 17:16:37 +0200

    Create PleaseSignin
    Create PleaseSignIn components that give note to sign in before sohwning CreateItem component

---
+++ frontend/components/PleaseSignIn.jsx
@@ -0,0 +1,22 @@
+import { Query } from 'react-apollo';
+import { CURRENT_USER_QUERY } from './User';
+import Signin from './Signin';
+
+const PleaseSignIn = props => (
+  <Query>
+    {(data, loading) => {
+      if (loading) return <p>Loading...</p>;
+      if (!data.me) {
+        return (
+          <div>
+            <p>Please Sign In before Continuning</p>
+            <Signin />
+          </div>
+        );
+      }
+      return props.children;
+    }}
+  </Query>
+);
+
+export default PleaseSignIn;
